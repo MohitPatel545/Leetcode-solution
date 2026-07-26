874c97e0fb3c18ee6c4767dc48cfc5db94b148f2
MohitPatel545 <mohit8577928483@gmail.com>
2026-07-26 07:51:49 +0530

    Time: 0 ms (100%), Space: 8.1 MB (70.48%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.length() != goal.length()) return false;
+        string c = s + s;
+        if(c.find(goal) != string::npos){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
